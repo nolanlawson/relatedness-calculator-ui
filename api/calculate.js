710d184bdd3446102dfc7415ddb4a4bc4b4cc072
Nolan Lawson <nolan@nolanlawson.com>
2022-03-13 12:05:19 -0700

    fix: use vercel serverless function proxy

---
+++ api/calculate.js
@@ -0,0 +1,20 @@
+const { parse } = require('url')
+const fetch = require('node-fetch')
+
+const URL = 'https://relatednesscalculatorapi.nolanlawson.com/RelatednessCalculator'
+
+module.exports = async (req, res) => {
+  try {
+    const { search } = parse(req.url)
+    const resp = await fetch(URL + search)
+    const text = await resp.text()
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.setHeader('Cache-Control', 'public,max-age=0,s-maxage=86400')
+    res.status(200)
+    res.send(text)
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
